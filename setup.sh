324319293a4f45bed101957f798c17fb8c225b84
TeeRev <trevor.p.hardcastle@gmail.com>
2023-05-23 10:39:44 +0100

    relative instead of absolute path, and container/image name variables in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+IMAGE_NAME=gfw-image
+CONTAINER_NAME=gfw-container
+
 # check if image exists
-if [[ "$(docker images -q gfw-image 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
     # build image from Dockerfile with name "gfw-image"
-    docker build -t gfw-image .;
+    docker build -t $IMAGE_NAME .;
 fi
 
 # check if container exists
-if [[ "$(docker ps -a -f name=gfw-container -q 2> /dev/null)" == "" ]]; then
+if [[ "$(docker ps -a -f name=$CONTAINER_NAME -q 2> /dev/null)" == "" ]]; then
     # create container with name "gfw-container" from image
-    docker run -d --gpus '"device=1"' -v /home/trev/repos/google-film-wrapper/:/home/repos/google-film-wrapper --name gfw-container gfw-image;
-
+    docker run -d --gpus '"device=1"' -v "${PWD}:/home/repos/google-film-wrapper" --name $CONTAINER_NAME $IMAGE_NAME;
 fi
 
 # check if directory exists in container
-if [[ "$(docker exec gfw-container ls /home/repos/frame-interpolation/pretrained_models 2> /dev/null)" == "" ]]; then
+if [[ "$(docker exec $CONTAINER_NAME ls /home/repos/frame-interpolation/pretrained_models 2> /dev/null)" == "" ]]; then
     # copy directory inside the container
-    docker exec gfw-container cp -r /home/repos/google-film-wrapper/pretrained_models /home/repos/frame-interpolation/
+    docker exec $CONTAINER_NAME cp -r /home/repos/google-film-wrapper/pretrained_models /home/repos/frame-interpolation/
 fi
 
 # log into the container
-docker exec -it gfw-container bash
+docker exec -it $CONTAINER_NAME bash
