25b3082c0c374ea12643239423bfad02608ac837
TeeRev <trevor.p.hardcastle@gmail.com>
2023-05-23 09:03:52 +0100

    debugging pretrained_models COPY command in setup.sh script

--- setup-debugging.sh
+++ setup-debugging.sh
@@ -9,13 +9,13 @@ fi
 # check if container exists
 if [[ "$(docker ps -a -f name=gfw-container -q 2> /dev/null)" == "" ]]; then
     # create container with name "gfw-container" from image
-    docker run -d --gpus '"device=1"' -v ~/repos/google-film-wrapper/:/home/repos/google-film-wrapper --name gfw-container gfw-image;
+    docker run -d --gpus '"device=1"' -v /home/repos/google-film-wrapper/:/home/repos/google-film-wrapper --name gfw-container gfw-image;
 fi
 
 # check if directory exists in container
 if [[ "$(docker exec gfw-container ls /home/repos/frame-interpolation/pretrained_models 2> /dev/null)" == "" ]]; then
     # copy directory inside the container
-    docker exec gfw-container cp -r ~/repos/google-film-wrapper/pretrained_models /home/repos/frame-interpolation/
+    docker exec gfw-container cp -r /home/repos/google-film-wrapper/pretrained_models /home/repos/frame-interpolation/
 fi
 
 # log into the container
